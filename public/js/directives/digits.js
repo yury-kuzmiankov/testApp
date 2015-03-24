45f544564a4187059a7d1d124fe304f647924dfc
yury-kuzmiankov <yury_kuzmiankov@epam.com>
2015-03-24 10:17:01 +0300

    Correct merge

---
+++ public/js/directives/digits.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yomanAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yomanAppApp
+ */
+angular.module('libraryApp')
+    .directive('digits', function () {
+        return {
+            restrict: "E",
+            scope: {
+                question : '=',
+                nextaction : '@'
+            },
+            templateUrl: 'templates/digits.html',
+            controller: function($scope, $element) {
+                $scope.selectCell = function() {
+                    if(this.cell.isSelected){
+                        this.cell.isSelected = false;
+                    }else{
+                        this.cell.isSelected = true;
+                    }
+                };
+                $scope.handleResults = function() {
+                    var selectedCount = 0;
+                    angular.forEach($scope.question.options, function(row) {
+                        angular.forEach(row, function(cell) {
+                            if (cell.isSelected) {
+                                selectedCount++;
+                            }
+                        });
+                    });
+                    $scope.question.results = selectedCount / $scope.question.options.length;
+                };
+            }
+        }
+    });
